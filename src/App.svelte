d92a7186245728e9ad9a02dc52073c2d5458a68b
borisivanko004 <borisivanko004@gmail.com>
2022-07-13 21:56:55 +0200

    v1.0.0

---
+++ src/App.svelte
@@ -0,0 +1,90 @@
+<script>
+  import Hangman from "./lib/Hangman.svelte"; 
+  import words from './words'
+
+  const word = [...words[Math.floor(Math.random() * words.length)]];
+  let holder = word.map(() => "-");
+
+  let playable = true
+
+  let lives = 11;
+
+  const handleInput = (e) => {
+    if (word.includes(e.target.value)) {
+      const matches = word.reduce(function (prev, curr, i) {
+        if (curr === e.target.value) prev.push(i);
+        return prev;
+      }, []);
+
+      for (let i = 0; i < matches.length; i++) {
+        console.log(matches[i]);
+        holder[matches[i]] = e.target.value;
+      }
+
+      if (holder.filter((letter) => letter === "-").length === 0) {
+        alert(`Gratulujem! Vyhrali ste... Slovo je ${word.join("")}`);
+        playable = false;
+      }
+
+    } else {
+      lives -= 1;
+
+      if (lives === 0) {
+        alert(`„Prehral si... Slovo je ${word.join("")}`);
+        playable = false;
+      }
+    }
+
+    e.target.value = "";
+  };
+</script>
+
+<main class='game'>
+<Hangman lives={lives}/>
+
+  <section class='holder'>
+    {holder.join(" ")}
+  </section>
+
+
+  <input type="text" on:input={handleInput} disabled='{!playable}' placeholder="Stlačením tu začnite hádať písmená" class='guess-input'/>
+  <button on:click={()=>location.reload()} class="reset">Resetovať</button>
+</main>
+
+<style>
+  .game {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+    margin: 0 auto;
+    width: clamp(10rem, 90vw, 30rem);
+  }
+
+  .guess-input {
+    padding: 0.5rem 1rem;
+    border-radius: 25rem;
+    border: 2px solid #9AEBA3;
+    outline: none;
+    margin: 1rem 0;
+    transition: .3s;
+    width: 100%;
+  }
+
+  .guess-input:focus {
+    border-color: #45C4B0;
+  }
+
+  .reset {
+    padding: 0.5rem 1rem;
+    border-radius: 25rem;
+    background: #45C4B0;
+    transition: .3s;
+    border: none;
+    width: 100%;
+  }
+
+  .reset:focus, .reset:hover {
+    background-color: #9AEBA3;
+  }
+</style>
